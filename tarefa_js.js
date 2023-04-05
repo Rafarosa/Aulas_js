ac24adc0582fe2baebdaa6d99123b0232b8a15f1
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2023-04-05 19:35:22 -0300

    Atualização dos itens feitos e aplicação de Logs

--- tarefa_js.js
+++ tarefa_js.js
@@ -1,24 +1,38 @@
-Exercícios
+// Exercícios
 //1 - Escreva uma função que some dois números e retorne o valor do resultado
 
-function soma(a,b){
-    return a +b
-} 
+function soma(numA, numB) {
+    // concatenação de strings:
+    // console.info('soma(' + numA.toString() + ', ' + numB.toString() + ')')
+    // interpolação de strings:
+    console.info(`soma(${numA}, ${numB})`) // mais recomendado neste caso
 
-let resultado = soma(10,23);
-console.log(resultado)
-//2 - Escreva uma função que permita uma pessoa saber quantos anos ela terá até o fim deste ano.
-
-function idadeFinalAno(anoDeNascimento){
-    let anoAtual = 2023;
-    let idade = anoAtual - anoDeNascimento;
-    let idadeFinalAno = idade + 1;
-    return idadeFinalAno;
+    return numA + numB
 }
 
-let minhaIdade = idadeFinalAno(1989);
-console.log(minhaIdade);
+let resultado = soma(10, 23);
+console.info(`resultado: ${resultado}`)
+
+console.info('--------------------------')
+
+
+//2 - Escreva uma função que permita uma pessoa saber quantos anos ela terá até o fim deste ano.
+
+function idadeFinalAno(anoDeNascimento) {
+    console.info(`idadeFinalAno(${anoDeNascimento})`)
+    // Date
+    let anoAtual = new Date().getFullYear();
+    console.debug(anoAtual)
+    let idade = anoAtual - anoDeNascimento
+    return idade;
+}
+
+let anoNascimento = 1999
+let minhaIdade = idadeFinalAno(anoNascimento);
+console.info(`Idade ao final do ano ${anoNascimento}: ${minhaIdade}`)
+
 //3 - Crie uma função que recebe como parâmetro  o nome, e o ano de nascimento de uma pessoa e diga quantos anos ela vai ter até o fim deste ano. Além disso, retorne um objeto contendo os atributos nome e idade.
+
 
 //4 - Escreva uma função que transforma uma string em minúscula (sem utilizar nenhum método)
 
